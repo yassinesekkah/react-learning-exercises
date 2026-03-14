71b598683ba0b467d9043229c56853817523e350
Yassine Sekkah <yassinesekkah11@gmail.com>
2026-03-14 19:56:48 +0000

    feat: add ex 5, 6, 7 and 8

---
+++ exercices/ex7.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+
+function App(){
+
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <h3>{count} </h3>
+
+      <button onClick={() => setCount(count + 1)}>
+        +
+      </button>
+
+      <button onClick={() => setCount(count - 1)}>
+        -
+      </button>
+
+      <button onClick={() => setCount(0)}>
+        reset
+      </button>
+    </div>
+  )
+}
+
+export default App;
